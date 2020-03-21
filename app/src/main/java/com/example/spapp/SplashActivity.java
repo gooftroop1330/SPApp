9a8c6cdbdf7167d53f39e56109bd2eda8ff9b290
Preston <prestonsmith657@gmail.com>
2020-03-21 15:43:34 -0400

    uh oh

--- app/src/main/java/com/example/spapp/SplashActivity.java
+++ app/src/main/java/com/example/spapp/SplashActivity.java
@@ -1,8 +1,11 @@
 package com.example.spapp;
 
+
 import android.content.Intent;
 import android.os.Bundle;
+
 import android.os.Handler;
+import android.provider.ContactsContract;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,37 +14,33 @@ import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+
 
 import java.io.BufferedReader;
+
 import java.io.FileNotFoundException;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 
-public class SplashActivity extends AppCompatActivity
-{
+public class SplashActivity extends AppCompatActivity {
     private static int SPLASH_SCREEN_TIMEOUT = 2000;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        createPositionJSON();
+        createPositionInfo();
 
         setContentView(R.layout.splash);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        new Handler().postDelayed(new Runnable()
-        {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void run()
-            {
+            public void run() {
 
                 Intent intent = new Intent(SplashActivity.this, WelcomeActivity.class);
 
@@ -51,42 +50,34 @@ public class SplashActivity extends AppCompatActivity
             }
         }, SPLASH_SCREEN_TIMEOUT);
 
-        MobileAds.initialize(this, new OnInitializationCompleteListener()
-        {
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus)
-            {
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
             }
         });
     }
 
-    public void createPositionJSON()
-    {
-        public void createPositionJSON() {
+    public void createPositionInfo() {
         String currLine = "";
         String split = ",";
-        JSONArray allPositionInfo = new JSONArray();
         try {
             InputStream is = getResources().openRawResource(R.raw.rescraped);
             BufferedReader br = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
             currLine = br.readLine();
-            while((currLine = br.readLine()) != null) {
+            while ((currLine = br.readLine()) != null) {
                 String[] position = currLine.split(split, 3);
                 position[1] = (position[1].split(". ", 2))[1];
-                JSONObject positionInfo = new JSONObject();
-                positionInfo.put("title", position[1]);
-                positionInfo.put("description", position[2]);
-                allPositionInfo.put(positionInfo);
+                //Push to Database
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
         }
+
     }
 }
+
 
 
 
